8b7851c52e2254661e4f698d3170ff3b44324396
lucas <paradisi.lucas@gmail.com>
2024-12-06 19:44:46 -0300

    Adding Point addition for p1 == p2

--- src/point.rs
+++ src/point.rs
@@ -41,7 +41,15 @@ impl Add for Point {
             return self
         }
         if self.x != None && other.x != None {
-            if self.x.clone().unwrap() == other.x.clone().unwrap() {
+            if self == other {
+                let s_num : BigInt = 3 * self.x.clone().unwrap() * self.x.clone().unwrap() + self.a.clone();
+                let s_den : BigInt = 2 * self.y.clone().unwrap();
+                let s = BigDecimal::from(s_num.clone()) / BigDecimal::from(s_den.clone());
+                let x : BigDecimal = s.clone() * s.clone() - 2 * self.x.clone().unwrap();
+                let y : BigDecimal = s.clone() * (self.x.clone().unwrap() - x.clone()) - self.y.unwrap();
+                return Self::new(x.to_bigint(), y.to_bigint(), self.a, self.b)
+            }
+            else if self.x.clone().unwrap() == other.x.clone().unwrap() {
                 return Self::new(None, None, self.a, self.b)
             } else {
                 let s_num = other.y.unwrap() - self.y.clone().unwrap();
@@ -97,6 +105,15 @@ mod tests {
         let p1 = Point::new(BigInt::from(2).into(), BigInt::from(5).into(), BigInt::from(5), BigInt::from(7));
         let p2 = Point::new(BigInt::from(-1).into(), BigInt::from(-1).into(), BigInt::from(5), BigInt::from(7));
         let p3 = Point::new(BigInt::from(3).into(), BigInt::from(-7).into(), BigInt::from(5), BigInt::from(7));
+
+        assert_eq!(p1 + p2, p3);
+    }
+
+    #[test]
+    fn add_points_with_p1_equal_to_p2() {
+        let p1 = Point::new(BigInt::from(-1).into(), BigInt::from(-1).into(), BigInt::from(5), BigInt::from(7));
+        let p2 = Point::new(BigInt::from(-1).into(), BigInt::from(-1).into(), BigInt::from(5), BigInt::from(7));
+        let p3 = Point::new(BigInt::from(18).into(), BigInt::from(77).into(), BigInt::from(5), BigInt::from(7));
 
         assert_eq!(p1 + p2, p3);
     }
